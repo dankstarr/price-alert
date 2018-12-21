bdc40c4101003b1cfe7aee30317f00cb924c0f91
Waseem <wasim.169105218@muj.manipal.edu>
2018-12-21 11:30:53 +0530

    fixed drive link converter

--- src/app.py
+++ src/app.py
@@ -5,27 +5,37 @@ from src.models.alerts.views import alert_blueprint
 import src.models.items.views as Items
 import src.models.stores.views as Stores
 
-
 from src.common.database import Database
-
 
 app = Flask(__name__)
 app.config.from_object('src.config')
-app.secret_key='123'
+app.secret_key = '123'
 
-app.register_blueprint(Users.user_blueprint, url_prefix ='/users')
-app.register_blueprint(alert_blueprint, url_prefix ='/alerts')
-app.register_blueprint(Items.item_blueprint, url_prefix ='/items')
-app.register_blueprint(Stores.store_blueprint, url_prefix ='/stores')
+app.register_blueprint(Users.user_blueprint, url_prefix='/users')
+app.register_blueprint(alert_blueprint, url_prefix='/alerts')
+app.register_blueprint(Items.item_blueprint, url_prefix='/items')
+app.register_blueprint(Stores.store_blueprint, url_prefix='/stores')
+
 
 @app.before_first_request
 def db_init():
     Database.initialize()
 
+
 @app.route('/')
 def home():
     return render_template('home.jinja2')
 
-@app.route('/drive')
+
+@app.route('/drive', methods=['POST', 'GET'])
 def drive():
-    return render_template('/drive/drivelink.jinja2', clip = pyperclip.paste())
+    if request.method == 'GET':
+        return render_template('/drive/drivelink.jinja2', clip=pyperclip.paste())
+    elif request.method == 'POST':
+        lines = request.form['lines']
+        all = lines.split('\n')
+        arr = []
+        for i in all:
+            arr.append("https://drive.google.com/file/d/" + i.split("id=")[1].split('&export=download')[0] + "/view")
+        print(arr)
+        return render_template('/drive/converted.jinja2', lines=arr)
